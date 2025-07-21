560cdad6906477b08ce74c1c8bf2fb5f29b73f26
levineye <levineye@gmail.com>
2025-07-21 16:30:19 +0300

    feat: add pass generator

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 type account struct {
 	login    string
 	password string
 	url      string
 }
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_=!@";
+// const specials = "-_=!@";
 
 func promptData(text string) string {
 	var res string
@@ -21,6 +28,22 @@ func outputAccount(acc account)  {
   fmt.Println(acc.login, acc.password, acc.url);
 }
 
+func generatePassword(length int) (string, error) {
+  if length < 0 {
+    return "", errors.New("PASSWORD_LENGTH");
+  }
+
+  newPass := make([]rune, length);
+
+  for index := range length {
+    randomIndex := rand.Intn(len(letters));
+    item := letters[randomIndex];
+    newPass[index] = rune(item);
+  }
+
+  return string(newPass), nil;
+}
+
 func main() {
   login := promptData("Введите логин: ");
   password := promptData("Введите пароль: ");
@@ -30,6 +53,14 @@ func main() {
     login: login,
     password: password,
     url: url,
+  }
+
+  if len(acc.password) == 0 {
+    newPass, passErr := generatePassword(10);
+
+    if passErr != nil {
+      acc.password = newPass;
+    }
   }
 
   outputAccount(acc);
